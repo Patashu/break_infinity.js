4f1a5280b51ca55b03d24e2ecc2050cd40bfe1da
Andrei Andreev <Razenpok@outlook.com>
2021-11-24 09:47:37 +0300

    Add test report generation

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  reporters: [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      pageTitle: "Test Report",
+      includeFailureMsg: true,
+      sort: "status"
+    }]
+  ]
 };
